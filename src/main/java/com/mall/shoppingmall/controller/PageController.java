b40e7f82a44773317b98e69bd0306cd6931286dd
yec3168 <yec3168@naver.com>
2023-12-07 16:51:17 +0900

    entity수정

--- src/main/java/com/mall/shoppingmall/controller/PageController.java
+++ src/main/java/com/mall/shoppingmall/controller/PageController.java
@@ -5,10 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
-
+    // main homepage
     @GetMapping("/")
     public String home(){
         return "index";
+    }
+
+    //회원가입
+    @GetMapping("/auth/signup")
+    public String signup(){
+        return "auth/signup";
     }
 
     // 로그인
--- src/main/java/com/mall/shoppingmall/entitiy/Member.java
+++ src/main/java/com/mall/shoppingmall/entitiy/Member.java
@@ -1,9 +1,12 @@
 package com.mall.shoppingmall.entitiy;
 
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-
-
+@NoArgsConstructor // 파라미터가 없는 디폴트 생성자를 생성
+@Getter
 @Entity
 public class Member {
     @Id
@@ -22,4 +25,11 @@ public class Member {
     @Column(length = 100, nullable = false)
     private String password; //password
 
+    @Builder
+    public Member(String name, String email, String phone_number, String password){
+        this.name = name;
+        this.email = email;
+        this.phone_number = phone_number;
+        this.password = password;
+    }
 }
